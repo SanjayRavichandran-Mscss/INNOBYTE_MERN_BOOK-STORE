f98b874de2746ba409c3319731ed3ac2ba5432e5
SanjayRavichandran-Mscss <117557388+SanjayRavichandran-Mscss@users.noreply.github.com>
2024-05-13 11:39:29 +0530

    change origin in cors

--- backend/index.js
+++ backend/index.js
@@ -58,7 +58,7 @@ app.use(express.json());
 
 app.use(
       cors({
-          origin:'http://localhost:5173',
+        origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
           methods:['GET','POST','PUT','DELETE'],
           allowedHeaders:['Content-Type'],
       })
