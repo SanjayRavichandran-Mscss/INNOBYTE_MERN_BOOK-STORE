074bafde67e55d379a14762a670c82a0ce7b4a76
SanjayRavichandran-Mscss <117557388+SanjayRavichandran-Mscss@users.noreply.github.com>
2024-05-14 08:46:37 +0530

    change port value in index.js

--- backend/index.js
+++ backend/index.js
@@ -87,6 +87,6 @@ mongoose
 // app.use("/", require("./routes/booksRoute"));
 app.use("/", booksRoute);
 
-const PORT = 5000;
+const PORT = 'https://innobyte-mern-book-store-backend.vercel.app';
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
 
